6ef09a74d6bc45699bf41161f96bc20f542f440e
HairAssassin <lturner@basecampcodingacademy.org>
2025-08-09 14:44:00 -0500

    changed clients to patients

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Customer:
     vaccinations: str
     insurance: str
 
-clients = [
+patients = [
     {"name": "Ariel", "birthday": 7/27/1997, "ssn": 4857}
     {"name": "Taylor", "birthday": 1/8/2003, "ssn": 2947}
     {"name": "Ariel", "birthday": 10/15/2006, "ssn": 9332}
@@ -18,10 +18,10 @@ vaccinations = {
     {"Flu", "RSV", "Covid-19", "Pneumonia", "Shingles"}
 }
 
-#def
-#def
-#def
-#def
+def
+def
+def
+def
 def main():
     print("Welcome to the Zed's Pharmacy!")
     print("What can I help you with")
