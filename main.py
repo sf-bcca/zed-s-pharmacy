8923ff571c0924994beb01dd99d41e45c336e9c4
HairAssassin <lturner@basecampcodingacademy.org>
2025-08-09 14:35:40 -0500

    removed hello world and added dataclass

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("Hello World")
+from dataclasses import dataclass
+
+@dataclass
+class Customer:
+    client_name: str
+    birthday: int
+    ssn: int
+    vaccinations: str
+    insurance: str
+
+clients = [
+    {"name": "Ariel", "birthday": 7/27/1997, "ssn": 4857}
+    {"name": "Taylor", "birthday": 1/8/2003, "ssn": 2947}
+    {"name": "Ariel", "birthday": 10/15/2006, "ssn": 9332}
+]
+
+vaccinations = {
+    {"Flu", "RSV", "Covid-19", "Pneumonia", "Shingles"}
+}
+
+def
+def
+def
+def
+def main():
+    print("Welcome to the Zed's Pharmacy!")
+    print("What can I help you with")
+
+
+
+
+
