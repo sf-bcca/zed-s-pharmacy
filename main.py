0239df261f6faeb7f59c0a5f85923677b4d32b08
ZNyla <zbean@basecampcodingacademy.org>
2025-08-12 19:19:52 -0500

    update #4

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ with open('Insurance - Sheet1.csv', mode = 'r', newline = '') as file:
 
 print(insurnace_dictionary)
 
-# vaccines_prices_dictionary = {}
+vaccines_prices_dictionary = {}
 
-# with open('Vaccines & Prices - Sheet1.csv', mode = 'r', newline = '') as file:
-#     reader = csv.DictReader(file)
-#     for row in reader:
-#         name = row['Name']
-#         price = row['Price']
-#         vaccines_prices_dictionary[name] = int(price)
+with open('Vaccines & Prices - Sheet1.csv', mode = 'r', newline = '') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        name = row['Name']
+        price = row['Price']
+        vaccines_prices_dictionary[name] = int(price)
 
-# print(vaccines_prices_dictionary)
+print(vaccines_prices_dictionary)
 
-#todo - read csv files for insurance, medication, and vacines
+
 
 @dataclass
 class Customer:
@@ -33,12 +33,6 @@ class Customer:
 vaccinations_type = [
     "Flu", "RSV", "Covid-19", "Pneumonia", "Shingles"
 ]
-
-# def calculate_total() => in: float, float. out: float -> total
-# def get_insurance_perc() => in: insurance_dictionary, str (user input insurance). out -> float
-# def get_med_price() => in: medication_dictionary, str (user input choice). out -> float
-# def get_vac_price() => in: vac_dictionary, str (user input choice). out -> float
-
 
 def get_patients_name(client_name: Customer) -> str:
     client_name = input("Name: ")
@@ -64,7 +58,9 @@ def get_vaccinations_type():
     
 def get_medicine_type():
     pass
-#todo
+
+def get_vac_price():
+    pass
 def main():
     print("Welcome to the Zed's Pharmacy!")
     print("May you sign in for us today?")
@@ -72,16 +68,18 @@ def main():
     birthday = get_birthday(birthday = int)
     ssn = get_ssn(ssn = int)
     print("What can we help you with today?")
-    choices = input("Vaccinations or Medicine?: ")
-    while True:
-        if choices == "Vaccinations":
-            vaccinations = get_vaccinations_type()
+    user_input = input("Vaccinations or Medicine?: ")
+    while user_input != "All Done":
+        if user_input == "Vaccinations":
+            get_vaccinations_type()
             print("Alrighty, all finished! Can I help you with anything else today?")
-            vaccinations = input("Vaccination, Medicine, Or All Done? ")
-        elif choices == "Medicine":
+            user_input = input("Vaccinations, Medicine, Or Done?")
+        elif user_input == "Medicine":
             pass
-        else:
+        elif user_input == "Done":
+            print(f"{vaccinations_type.remove}")
             break
+
 
         
 
